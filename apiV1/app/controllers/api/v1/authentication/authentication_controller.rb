0f5d78051939bb06ad4494c4e35127fb54eee2ee
MP.Anselin <mackendetpierro@gmail.com>
2019-12-13 11:56:31 +0100

    connect with firebase databse and get simple requets

--- apiV1/app/controllers/api/v1/authentication/authentication_controller.rb
+++ apiV1/app/controllers/api/v1/authentication/authentication_controller.rb
@@ -1,14 +1,20 @@
 class Api::V1::Authentication::AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
 
+  @@BASE_URI = 'https://totem2-f3e2e.firebaseio.com/'
+  @@PRIVATE_KEY_JSON_STRING = File.open('app/controllers/api/v1/resources/totem2-f3e2e-firebase-adminsdk-lfqcp-3e7ed4ba43.json').read
+
   # POST /authenticate
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
+    path = 'https://totem2-f3e2e.firebaseio.com/users_administrator/6FgDbWaK6ciQTTDcdGbz/email'
+    firebase_manager = Api::V1::Authentication::FirebaseManager.new
 
     if command.success?
-      render json: { auth_token: FirebaseManager.get('https://totem2-f3e2e.firebaseio.com/users_administrator/6FgDbWaK6ciQTTDcdGbz/email') }
+      render json: { auth_token: firebase_manager.get(path) }
+      #render json: {auth_token: path}
     else
-      render json: { error: command.errors }, status: :unauthorized
+      render json: {error: command.errors}, status: :unauthorized
     end
   end
 end
--- apiV1/app/controllers/api/v1/authentication/firebase_manager.rb
+++ apiV1/app/controllers/api/v1/authentication/firebase_manager.rb
@@ -1,13 +1,30 @@
-class Api::V1::Authentication::FirebaseManager < ApplicationController
+class Api::V1::Authentication::FirebaseManager
 
-  BASE_URI = 'https://totem2-f3e2e.firebaseio.com/'
-  PRIVATE_KEY_JSON_STRING = File.open('app/controllers/api/v1/resources/totem2-f3e2e-firebase-adminsdk-lfqcp-3e7ed4ba43.json').read
+  @@BASE_URI = 'https://totem2-f3e2e.firebaseio.com/'
+  @@PRIVATE_KEY_JSON_STRING = File.open('app/controllers/api/v1/resources/totem2-f3e2e-firebase-adminsdk-lfqcp-3e7ed4ba43.json').read
 
   def initialize
-    @firebase = Firebase::Client.new(BASE_URI, PRIVATE_KEY_JSON_STRING)
+    @firebase = Firebase::Client.new(@@BASE_URI, @@PRIVATE_KEY_JSON_STRING)
   end
 
   def get(path)
     @firebase.get(path)
   end
+
+  def set(path, data)
+    @firebase.set(path, data)
+  end
+
+  def delete(path)
+    @firebase.delete(path)
+  end
+
+  def push(path, data)
+    @firebase.push(path, data)
+  end
+
+  def update(path, data)
+    @firebase.update(path, data)
+  end
+
 end
