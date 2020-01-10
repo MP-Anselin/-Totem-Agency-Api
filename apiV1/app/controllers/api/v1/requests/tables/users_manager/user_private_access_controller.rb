46698ef3f16c4d94ddf5bf773f2033dac7eb9283
MP.Anselin <mackendetpierro@gmail.com>
2020-01-10 19:05:12 +0100

    reset password

--- apiV1/app/controllers/api/v1/requests/tables/users_manager/user_private_access_controller.rb
+++ apiV1/app/controllers/api/v1/requests/tables/users_manager/user_private_access_controller.rb
@@ -6,36 +6,53 @@ module Api
         module UsersManager
           # Class to manage private information of the user
           class UserPrivateAccessController < Api::V1::FirebaseRequests::FireDatabaseStore
-            #SIGN_UP_URL = FIREBASE_SING_UP_USER + FIREBASE_API_KEY
 
             def initialize
               super
               @http_requests = Api::V1::Requests::HttpRequests::HttpRequestsManager.new
+              @params = nil
+              @path = nil
             end
 
-            # POST /sign up and sign in
-            def sign_up
+            # exec curl and render on right way the answer
+            def render_action
+              response = @http_requests.curl_exe(@path, @params)
+              render json: response.body, status: response.code
+              @params = nil
+              @path = nil
+            end
+
+            # POST /sign up and sign in with email and password, allow anonymouse connection
+            def sign_ep
               email = params[:email]
               password = params[:password]
               registered = params[:registered]
               id_params = {'email': email, 'password': password, 'returnSecureToken': true}
-              params = !password.nil? ? id_params : {'returnSecureToken': true}
-              path = registered && !password.nil? ? FIREBASE_SING_IN_USER : FIREBASE_SIGN_UP_USER
-              response = @http_requests.curl_exe(path, params)
-              render json: response.body, status: response.code
+
+              @params = !password.nil? ? id_params : {'returnSecureToken': true}
+              @path = registered && !password.nil? ? FIREBASE_SING_IN_USER : FIREBASE_SIGN_UP_USER
+              render_action
             end
 
-            # POST sign in
-            def sign_in
+            # POST /user/id/pwd/reset/code/email send email to reset password user
+            def reset_pwd_send_email
               email = params[:email]
-              password = params[:password]
-              response = @http_requests.curl_exe(FIREBASE_SING_IN_USER,
-                                                 'email': email,
-                                                 'password': password,
-                                                 'returnSecureToken': true)
-              render json: response.body, status: response.code
+              request_type = params[:requestType]
+
+              @params = {'requestType': request_type, 'email': email}
+              @path = FIREBASE_CODE_PWD_RESET_USER
+              render_action
             end
 
+            # POST /user/id/pwd/reset/code/confirmation check if password was changed
+            def pwd_reset_verification
+              oob_code = params[:oobCode]
+              new_pwd = params[:newPassword]
+
+              @params = new_pwd.nil? ? {'oobCode': oob_code} : {'oobCode': oob_code, 'newPassword': new_pwd}
+              @path = FIREBASE_PWD_RESET_USER
+              render_action
+            end
 
           end
         end
--- apiV1/config/routes.rb
+++ apiV1/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :basic_tests
   post 'client/authentication', to: 'api/v1/authentication/authentication#authenticate'
-  post '/user/signxx', to: 'api/v1/requests/tables/users_manager/user_private_access#sign_up'
+  post '/user/signep', to: 'api/v1/requests/tables/users_manager/user_private_access#sign_ep'
+  post '/user/id/pwd/reset/code/email', to: 'api/v1/requests/tables/users_manager/user_private_access#reset_pwd_send_email'
+  post '/user/id/pwd/reset/code/confirmation', to: 'api/v1/requests/tables/users_manager/user_private_access#pwd_reset_verification'
 
   namespace :api do
     namespace :v1 do
